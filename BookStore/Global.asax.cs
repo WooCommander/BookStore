6adc89a34aadddd449a9b7a42acb17facb29e6b2
ocnelavoc <ocnelavoc@gmail.com>
2020-02-19 07:02:51 +0200

    Добавьте файлы проекта.

---
+++ BookStore/Global.asax.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using BookStore.Models;
+using System.Data.Entity;
+using BookStore.Logick;
+
+namespace BookStore
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Database.SetInitializer(new BookDbInitializer());
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
